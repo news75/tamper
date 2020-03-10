4c960f3c8377317f4625ecd99dd540bd9ae3637f
giuseppe <giuseppe.carnevali@trovaprezzi.it>
2020-03-10 08:59:34 +0100

    extra hello

---
+++ hello.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.trovaprezzi.it/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert('hello world! From Git!!');
+})();
